0f590dc63b8de8baf58bb14f26895f1f5675dc58
Martí Bosch <marti.bosch@protonmail.com>
2023-04-12 12:09:04 +0200

    feat: initial commit

---
+++ tests/test_bake_project.py
@@ -0,0 +1,89 @@
+import os
+import shlex
+import subprocess
+from contextlib import contextmanager
+from typing import List
+
+from cookiecutter.utils import rmtree
+
+# logging.basicConfig(level=logging.DEBUG)
+
+
+@contextmanager
+def inside_dir(dirpath):
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
+
+def run_inside_dir(commands, dirpath):
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param commands: Commands that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    with inside_dir(dirpath):
+        for command in commands:
+            subprocess.check_call(shlex.split(command))
+
+
+def check_output_inside_dir(command, dirpath):
+    """Run a command from inside a given directory, returning the command output"""
+    with inside_dir(dirpath):
+        return subprocess.check_output(shlex.split(command))
+
+
+def execute(command: List[str], dirpath: str, timeout=30, supress_warning=True):
+    """Run command inside given directory and returns output
+
+    if there's stderr, then it may raise exception according to supress_warning
+    """
+    with inside_dir(dirpath):
+        proc = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    out, err = proc.communicate(timeout=timeout)
+    out = out.decode("utf-8")
+    err = err.decode("utf-8")
+
+    if err and not supress_warning:
+        raise RuntimeError(err)
+    else:
+        print(err)
+        return out
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies) as result:
+        assert result.project.isdir()
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert "pyproject.toml" in found_toplevel_files
+        assert "tox.ini" in found_toplevel_files
+        assert "tests" in found_toplevel_files
+
+        # test that pre-commit runs without error for the generated project
+        run_inside_dir(["git", "init"], result.project)
+        run_inside_dir(["pre-commit", "run", "--all-files"], result.project)
---
+++ {{cookiecutter.repo_name}}/docs/api.md
@@ -0,0 +1,6 @@
+# API
+
+```{eval-rst}
+.. automodule:: {{ cookiecutter.project_slug }}
+   :members:
+```
